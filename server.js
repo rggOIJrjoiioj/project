3cd0fea9896541151bf16a1b5606f010479d7dd1
pipikaka <nakasidzesepa@gmail.com>
2024-11-27 22:29:45 +0300

    On branch main

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-let feedbacks = []; // Массив для хранения отзывов
+let feedbacks = [];
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Эндпоинт для получения всех отзывов
+
 app.get('/feedbacks', (req, res) => {
     res.json(feedbacks);
 });
 
-// Эндпоинт для отправки отзыва
+
 app.post('/feedbacks', (req, res) => {
     const feedback = req.body;
     feedbacks.push(feedback);
